46a21098fb1c9ece3f19864dbd43a3c7adf8cd26
Alba Giraldo <agiraldomolina@gmail.com>
2024-10-12 10:26:32 -0400

    Improving reusability with  Props

--- src/index.js
+++ src/index.js
@@ -4,11 +4,36 @@ import ReactDOM from 'react-dom/client';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test(){
+  const [movieRating, setMovieRating] = React.useState(0);
+
+  return (
+    <div>
+      <StarRating 
+        color ={"blue"} 
+        maxRating={10}
+        onSetRating={setMovieRating} 
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
-    <StarRating maxRating={5} />
+    <StarRating 
+      maxRating={10} 
+      messages ={['Terrible','Bad', 'Okay', 'Good', 'Amazing']} 
+    />
+    <StarRating 
+      size={24}  
+      color={"#3a833d"} 
+      className ={'test'} 
+      messages ={['Terrible','Bad', 'Okay', 'Good', 'Amazing']} 
+      defaultRating={3}
+    />
+    <Test />
   </React.StrictMode>
 );
