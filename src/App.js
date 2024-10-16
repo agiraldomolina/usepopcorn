b3bad02091069440223a667653dbe61143e1642b
Alba Giraldo <agiraldomolina@gmail.com>
2024-10-16 14:51:06 -0400

    Synchronizing queries  with movie data

--- src/App.js
+++ src/App.js
@@ -51,14 +51,30 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 const KEY = "d9a817c0";
-// const query = "inception";
-const query = "jjhggg";
+const tempQuery = "inception";
+// const query = "jjhggg";
 
 export default function App() {
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+
+  // useEffect(function(){
+  //   console.log('After the initial render')
+  // })
+
+  // useEffect(function(){
+  //   console.log('After every render')
+  // }, [])
+
+  // useEffect(function(){
+  //   console.log("D")
+  // },[query])
+
+  // console.log('During render')
+ 
 
   useEffect(function () {
     async function fetchMovies(){
@@ -66,20 +82,15 @@ export default function App() {
         setIsLoading(true);
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}&type=movie`);
 
-        // if (!res.ok) {
-        //   throw new Error(`HTTP error! status: ${res.status}`);
-        // }
         const data = await res.json();
-        console.log('data:', data)
 
         if (data.Response === "False") throw new Error ("Movie Not Found")
 
         setMovies(data.Search);
   
       } catch(err) {
-        console.log("Mensaje desde catch:"+ err.message)
         if (err.message ==="Failed to fetch"){
           setError ("Somethig went wrong, please try again later.")
         }
@@ -90,8 +101,14 @@ export default function App() {
         setIsLoading(false);
       }
     }
+
+    if (query.length < 3) {
+      setMovies([]);
+      setError("");
+      return
+    }
     fetchMovies();
-  }, []);
+  }, [query]);
 
 
 
@@ -99,7 +116,7 @@ export default function App() {
     <>
       <NavBar  >
         <Logo />
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movies={movies} />
       </NavBar>
       <Main>
@@ -157,8 +174,7 @@ function Logo(){
   )
 }
 
-function Search(){
-  const [query, setQuery] = useState("");
+function Search({query, setQuery}){
 
   return (
     <input
